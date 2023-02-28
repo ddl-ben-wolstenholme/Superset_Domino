c571c13c9924d7ca86da64b5c6b7b2c03d05b074
ddl-ben-wolstenholme <103590138+ddl-ben-wolstenholme@users.noreply.github.com>
2023-02-28 11:06:48 +0000

    Update start_superset.sh

--- start_superset.sh
+++ start_superset.sh
@@ -29,4 +29,4 @@ else
 fi
 
 echo "Starting up Superset"
-(superset run --host "0.0.0.0" --port 8088 --with-threads --reload $ADDITIONAL_LAUNCH_OPTIONS 3>&1 1>&2 2>&3 | grep -v DEBUG\: |grep -v WARN\:  | grep -v INFO\:) 3>&1 1>&2 2>&3
+(superset run --host "0.0.0.0" --port 8088 --with-threads --reload $ADDITIONAL_LAUNCH_OPTIONS 3>&1 1>&2 2>&3 | grep -v INFO\:) 3>&1 1>&2 2>&3
